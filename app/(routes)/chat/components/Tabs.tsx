ee3ebd2d959271696e4d5d4f39f5e44fd034407c
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-06-20 17:11:50 +0530

    fixed homepage

--- app/(routes)/chat/components/Tabs.tsx
+++ app/(routes)/chat/components/Tabs.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import {Tabs, Tab} from "@nextui-org/react";
+import { Tabs, Tab } from "@nextui-org/react";
 import { MessageSquareText, Video } from 'lucide-react';
-export default function App() {
 
+export default function App() {
   return (
     <div className="flex flex-wrap gap-4">
-        <Tabs key={"danger"} color={"danger"} aria-label="Tabs colors" radius="full">
-          <Tab key="msg" title={<MessageSquareText/>}/>
-          <Tab key="video" title={< Video />}/>
-        </Tabs>
+      <Tabs
+        key="default"
+        aria-label="Tabs colors"
+        radius="full"
+        className="custom-tabs"
+      >
+        <Tab key="msg" title={<MessageSquareText />} />
+        <Tab key="video" title={<Video />} />
+      </Tabs>
     </div>
   );
 }
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -8,10 +8,10 @@ const Hero = () => {
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             <div className="flex flex-col justify-center items-center px-[7vw] gap-2 lg:flex-row lg:gap-10">
                 <div className="flex flex-col justify-between items-center lg:w-[70%]">
-                    <p className="text-[5vh] md:text-[6vh] lg:text-[7vh] py-2 font-bold relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+                    <p className="text-[5vh] md:text-[6vh] lg:text-[7vh] py-2 font-bold relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400">
                         Collaborate seamlessly, create effortlessly
                     </p>
-                    <p className="mt-4 text-lg">
+                    <p className="mt-4 text-lg font-bold relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400">
                         Transform collaboration with real-time editing of code, documents, and spreadsheets. Enjoy intuitive task management, chat, video calls, and secure cloud deployment. Accomplish
                         more, together.
                     </p>
