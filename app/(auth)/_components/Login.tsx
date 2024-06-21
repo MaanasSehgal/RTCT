e8cf8f030f23abad4a245b9a6510bc7b03a90ad8
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-06-22 03:33:15 +0530

    Styled auth components

--- app/(auth)/_components/Login.tsx
+++ app/(auth)/_components/Login.tsx
@@ -1,27 +1,58 @@
 "use client";
-import React from "react";
-import {IconBrandGithub, IconBrandGoogle} from "@tabler/icons-react";
+import React, {useState} from "react";
+import {IconBrandGithub, IconBrandGoogle, IconEye, IconEyeOff} from "@tabler/icons-react";
 import {cn} from "@/app/utils/cn";
 import Link from "next/link";
 import {Label} from "./Label";
 import {Input} from "./Input";
 
 export function LoginForm() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        showPassword: false, // Track whether password is visible or not
+    });
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("Form submitted");
     };
+
+    const togglePasswordVisibility = () => {
+        setFormData((prevState) => ({
+            ...prevState,
+            showPassword: !prevState.showPassword,
+        }));
+    };
+
     return (
         <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-6 md:p-10 shadow-input bg-white dark:bg-black">
             <h2 className="font-bold text-2xl text-white">Login</h2>
             <form className="my-8" onSubmit={handleSubmit}>
                 <LabelInputContainer className="mb-4">
-                    <Label htmlFor="email">Email Address</Label>
-                    <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+                    <Label htmlFor="email">
+                        Email Address <span className="text-red-500">*</span>
+                    </Label>
+                    <Input id="email" placeholder="projectmayhem@fc.com" type="email" required />
                 </LabelInputContainer>
                 <LabelInputContainer className="mb-4">
-                    <Label htmlFor="password">Password</Label>
-                    <Input id="password" placeholder="••••••••" type="password" />
+                    <Label htmlFor="password">
+                        Password <span className="text-red-500">*</span>
+                    </Label>
+                    <div className="relative">
+                        <Input
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={(e) => setFormData((prevState) => ({...prevState, password: e.target.value}))}
+                            placeholder="••••••••"
+                            type={formData.showPassword ? "text" : "password"}
+                            required
+                        />
+                        <button type="button" className="absolute inset-y-0 right-0 pr-3 flex items-center focus:outline-none" onClick={togglePasswordVisibility}>
+                            {formData.showPassword ? <IconEyeOff /> : <IconEye />}
+                        </button>
+                    </div>
                 </LabelInputContainer>
 
                 <button
@@ -35,14 +66,14 @@ export function LoginForm() {
 
                 <div className="flex flex-col space-y-4">
                     <button
-                        className=" relative group/btn flex space-x-2 justify-center items-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        className="relative group/btn flex space-x-2 justify-center items-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                         type="submit">
                         <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                         <span className="text-neutral-700 dark:text-neutral-300 text-sm">Google</span>
                         <BottomGradient />
                     </button>
                     <button
-                        className=" relative group/btn flex space-x-2 justify-center items-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        className="relative group/btn flex space-x-2 justify-center items-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                         type="submit">
                         <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                         <span className="text-neutral-700 dark:text-neutral-300 text-sm">GitHub</span>
@@ -50,13 +81,15 @@ export function LoginForm() {
                     </button>
                 </div>
                 <div className="mt-4">
-                    Not registered yet? <Link className="text-blue-500" href="/signup">Create an account</Link>{" "}
+                    Not registered yet?{" "}
+                    <Link className="text-blue-500" href="/signup">
+                        Create an account
+                    </Link>
                 </div>
             </form>
         </div>
     );
 }
-
 const BottomGradient = () => {
     return (
         <>
