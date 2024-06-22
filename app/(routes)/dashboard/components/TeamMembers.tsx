d208fa5368726cdab6f7be150043118a90d71fd8
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-06-22 17:13:08 +0530

    Configured team members page

--- app/(routes)/dashboard/components/TeamMembers.tsx
+++ app/(routes)/dashboard/components/TeamMembers.tsx
@@ -109,22 +109,28 @@ const TeamMembers = () => {
             role: "viewer"
         }
     ];
-    
+
     return (
-        <div>
-            {users.map((user) => {  
+        <div className="w-full mt-16 h-[80%] flex flex-col items-center gap-4 overflow-y-auto">
+            {/* <div className="bg-transparent my-2 w-4/5 flex gap-2">
+                <Image src="/userlogo.png" alt="logo" width={24} height={24} />
+                <h1>John Doe (Owner)</h1></div> */}
+            <h1 className="text-3xl font-bold">Manage Team Members</h1>
+            {users.map((user) => {
                 return (
-                    <div className="flex justify-between items-center">
-                        <div className="w-4/5 h-10 bg-gray-500 my-2">
-                            <div>
-                                <Image src={user.avatar} alt="logo" width={18} height={18}/> 
-                                {user.name}
-                            </div>
+                    <div className="w-4/5 rounded-full flex items-center justify-between bg-black p-2">
+                        <div className="flex items-center gap-8 p-3">
+                            <Image className="w-12" src={user.avatar} alt="logo" width={24} height={24} />
+                            <h2 className="text-md">{user.name}</h2>
+                        </div>
+                        <div className="text-white cursor-pointer p-3">
+                            ...
                         </div>
                     </div>
                 )
             })}
         </div>
+
     )
 }
 
