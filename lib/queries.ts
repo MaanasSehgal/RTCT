0fcb9395f85ac9927821b1e8a05243175bc5582b
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-06-29 00:36:48 +0530

    removed convex (#16)
    * finished integrating chat
    
    * Empty commit
    
    * removed convex

--- lib/queries.ts
+++
@@ -1,6 +0,0 @@
-import {useMutation} from "convex/react";
-import {api} from "@/convex/_generated/api";
-
-const createUser = useMutation(api.user.createUser);
-const createTeam = useMutation(api.teams.createTeam);
-
