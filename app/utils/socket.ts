31e0edf17d2d1c9c88372bf246f8ea338ed1a076
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-06-29 17:37:34 +0530

    fixed socket.io url

--- app/utils/socket.ts
+++ app/utils/socket.ts
@@ -1,8 +1,8 @@
 import {io} from "socket.io-client";
 import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 // "undefined" means the URL will be computed from the `window.location` object
-// const URL = "https://mrflyn.krak8.xyz" || "";
-const URL = "http://localhost:8000" || "";
+const URL = "https://mrflyn.krak8.xyz" || "";
+// const URL = "http://localhost:8000" || "";
 
 export const socket = io(URL, {
     autoConnect: false,
